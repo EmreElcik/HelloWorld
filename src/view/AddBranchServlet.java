dc3c3a1a2fc6a22370328001ce855a1e5c858c63
eelcik <emre.elcik@allianz.com.tr>
2019-02-18 17:34:20 +0300

    Deneme Todo kaldirildi

--- src/view/AddBranchServlet.java
+++ src/view/AddBranchServlet.java
@@ -32,7 +32,6 @@ public class AddBranchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		PrintWriter out = response.getWriter();
 		
 		out.println("<html>"
